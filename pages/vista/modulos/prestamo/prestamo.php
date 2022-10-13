d1a9d1e7b733e3c4b87e547aee95f0f800c6b6ca
adonay-ar <ar20012@ues.edu.sv>
2022-10-13 00:24:55 -0600

    Mostrar prestamos

--- pages/vista/modulos/prestamo/prestamo.php
+++ pages/vista/modulos/prestamo/prestamo.php
@@ -1,13 +1,10 @@
 <div class="container-xxl flex-grow-2 container-p-y">
-    <div class="col-md-12">
-        <div class="row">
-            <div class="col-md-12">
-                <h4 class="fw-bold py-1 mb-4"><span class="text-muted fw-light">Préstamos /</span> Listado de Préstamos</h4>
-            </div>
-            
-        </div>
+
+    <div class="d-flex align-items-center justify-content-between py-3">
+        <h4 class="fw-bold py-1 m-0"><span class="text-muted fw-light">Préstamos /</span> Listado de Préstamos</h4>
+        <button class="btn btn-primary" type="button"><span class="tf-icons bx bx-plus-circle"></span> Nuevo</button>
     </div>
-    
+
     <!-- Basic Bootstrap Table -->
     <div class="card">
     <div class="card-header col-md-12">
@@ -16,12 +13,6 @@
             <div class="col-md-4">
                 <input type="text" class="form-control" placeholder="Buscar...">
             </div>
-
-            <div class="col-md-2">
-                <div class="input-group">
-                    <button class="btn btn-primary" type="button"><span class="tf-icons bx bx-plus-circle"></span> Nuevo</button>
-                </div>
-            </div>
         </div>
     </div>
     <div class="table-responsive text-nowrap">
@@ -29,35 +20,32 @@
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Client</th>
-                <th>Users</th>
-                <th>Status</th>
-                <th>Actions</th>
-                <th>Actions</th>
+                <th>Código socio</th>
+                <th>Socio</th>
+                <th>Monto Inicial</th>
+                <th>Fecha de Emisión</th>
+                <th>Estado</th>
+                <th>Acciones</th>
             </tr>
         </thead>
         <tbody class="table-border-bottom-0">
-            <tr>
-                <td></td>
-                <td>Albert Cook</td>
-                <td></td>
-                <td><span class="badge bg-label-primary me-1">Activo</span></td>
-            <td>
-                <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                    <i class="bx bx-dots-vertical-rounded"></i>
-                </button>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="javascript:void(0);"
-                    ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                    >
-                    <a class="dropdown-item" href="javascript:void(0);"
-                    ><i class="bx bx-trash me-1"></i> Delete</a
-                    >
-                </div>
-                </div>
-            </td>
-            </tr>
+            <?php 
+                include_once '../modelo/dao/daoPrestamo.php';
+                
+                $prestamos = getPrestamos();
+
+                foreach($prestamos as $prestamo){
+                    echo '<tr>';
+                    echo '<td>'.$prestamo['id_prestamo'].'</td>';
+                    echo '<td>'.$prestamo['cod_socio'].'</td>';
+                    echo '<td>'.$prestamo['nombre_socio'].'</td>';
+                    echo '<td>$'.number_format($prestamo['monto_prestamo']).'</td>';
+                    echo '<td>'.$prestamo['fecha_emision_prestamo'].'</td>';
+                    echo '<td><span class="badge bg-label-warning me-1">Pendiente</span></td>';
+                    echo '<td><a class="text-info" href="prestamo.php?accion=editar&id='.$prestamo['id_prestamo'].'"><span class="tf-icons bx bx-edit"></span></a> <a class="text-danger" href="prestamo.php?accion=eliminar&id='.$prestamo['id_prestamo'].'"><span class="tf-icons bx bx-trash"></span></a></td>';
+                    echo '</tr>';
+                }
+            ?>
         </tbody>
         </table>
     </div>
